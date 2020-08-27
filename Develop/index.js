a658368b58b6656b941fee14fa838a4930c78676
vsteere <vssteere@gmail.com>
2020-08-27 12:18:05 -0500

    added a list of initial questions to array

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,14 @@
 // array of questions for user
 const questions = [
+    "What is the title of your project?",
+     "Please enter a brief description of your project",
+     "Please enter installation instructions for the application",
+     "Please enter usage information",
+     "Please enter contribution guidelines for project",
+     "Please add test instructions",
+     //need a list of options for licenses
+     "Please enter your GitHub username",
+     "Please enter your preferred email address"
 
 ];
 
